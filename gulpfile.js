192a208822104b0e353488914ef5f0cfc683b05c
Serhii Yakymenko <64821812+serhii-edit@users.noreply.github.com>
2021-05-19 12:58:59 -0400

    Gulp task (watch Html-reload)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
-const gulp = require("gulp");
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
 
 gulp.task("hello", function(done) {
   console.log("Hello World!");
 
   done();
 })
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+
+  gulp.watch("./*.html").on('change', browserSync.reload);
+  
+  gulp.watch("./css/*.css").on('change', browserSync.reload);
+
+});
