e906e3566f56848c822f7e57ea933d84cff260da
Serhii Yakymenko <64821812+serhii-edit@users.noreply.github.com>
2021-05-20 18:34:05 -0400

    Feat: add gulp-dart-sass in gulpfile.js (other gulp-sass added in dependencies  but not functioning), fix code at gulpfile.js for sass, and Static server

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,31 +1,40 @@
-var gulp = require("gulp");
+var {src, dest, watch} = require("gulp");
+var sass = require('gulp-dart-sass');
 var browserSync = require("browser-sync").create();
 
-var cssmin = require("gulp-cssmin");
-var rename = require('gulp-rename');
+// var cssmin = require("gulp-cssmin");
+// var rename = require('gulp-rename');
 
-
-gulp.task("hello", function(done) {
-  console.log("Hello World!");
-
-  done();
-})
-
-gulp.task('browser-sync', function() {
+// Static server
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
 
-  gulp.watch("./*.html").on('change', browserSync.reload);
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.scss", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
 
-});
 
-gulp.task("min-css", function(done){
-  gulp.src("./css/*.css")
-  .pipe(cssmin())
-  .pipe(rename({suffix: ".min"}))
-  .pipe(gulp.dest("./../dist/css/"));
-  done();
-});
+};
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+  return src("./sass/*.scss")
+      .pipe(sass())
+      .pipe(dest("./css/"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
+
+// gulp.task("min-css", function(done){
+//   gulp.src("./css/*.css")
+//   .pipe(cssmin())
+//   .pipe(rename({suffix: ".min"}))
+//   .pipe(gulp.dest("./../dist/css/"));
+//   done();
+// });
