a0e60907577d863620790ab6d8f19c8e09435b9c
Serhii Yakymenko <64821812+serhii-edit@users.noreply.github.com>
2021-05-28 16:26:28 -0400

    Feat: Add modal window for request call form

---
+++ src/js/main.js
@@ -0,0 +1,81 @@
+// var name = "Serhii!";
+// var age = 17;
+
+// console.log(name + " " + age)
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// var pass = false;
+// if (7 > 6) {
+//   console.log("Success!")
+// } else {
+//   console.log ("Wrong!");
+// }
+
+// var hero = document.querySelector(".hero");
+// console.log(hero);
+
+// console.log(typeof(age));
+
+// var serhii = {
+//   name: "Serhii",
+//   age: 17,
+//   hair: "brown",
+//   eyes: "breen",
+//   resident: "UKR/USA",
+//   address: "1220 State Street",
+//   social: 511551511,
+//   wife: {
+//     name: "Ezabella"
+//   }
+// }
+
+// console.log(serhii);
+// console.log(serhii.age)
+
+// for (var i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// console.log("new value of i ",
+//  i);
+// console.log("new value of i " + i)
+
+// var itemsList = document.querySelectorAll('.card__items');
+
+// console.log(itemsList[0].classList)
+
+
+// itemsList.forEach(item => {
+//   item.classList.add("from-javascript");
+// });
+
+// function getSum(x, y) {
+//   console.log(x + y)
+// }
+
+// getSum(5, 10);
+
+// var hello = (name, age) => {
+//   console.log("Hello " + name + ". Your " + age + " age?");
+// };
+
+// hello("Serhii", 17);
+
+// button = document.querySelector(".logo");
+// console.log(button);
+
+// button.addEventListener("click", function (event) {
+//   console.log("This class is " + this.classList);
+// });
+
+// document.addEventListener("keypress", function(event) {
+//   // console.log("You press on " + event.key);
+//   if(event.code == "Space") {
+//     console.log("You clicked on space");
+//   } else {
+//     console.log("event.key");
+//   }
+// });
