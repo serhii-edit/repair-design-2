f72072c423cde56c4f5ebd2511499097e7b56e17
Serhii Yakymenko <64821812+serhii-edit@users.noreply.github.com>
2021-05-30 16:18:08 -0400

    Feat: JQuery script fadeIn & Out arrow-top

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function(event){
+document.addEventListener("DOMContentLoaded", function(event){
   const modal = document.querySelector(".modal");
   // const modalBtn = document.querySelectorAll("[data-toggle=modal]");
   // const closeBtn = document.querySelector(".modal__close");
@@ -24,23 +24,26 @@
 //     };
 //   });
 
-// });
 
-// ES5 > ES6, use of babel
-window.onclick = function (e) {
-  if(e.target == modal) {
-    modal.classList.toggle("modal--visible");
+  // ES5 > ES6, use of babel
+  window.onclick = function (e) {
+    if(e.target == modal) {
+      modal.classList.toggle("modal--visible");
+    };
   };
-};
 
-window.addEventListener("keydown", function (event) {
-  if(event.key == 'Escape') {
-    modal.classList.remove("modal--visible");
-  };
+  window.addEventListener("keydown", function (event) {
+    if(event.key == 'Escape') {
+      modal.classList.remove("modal--visible");
+    };
+  });
+
 });
 
 
 $(document).ready(function () {
+
+// Modal window, close and open
   var modal = $(".modal");
   var modalBtn = $("[data-toggle=modal]");
   var closeBtn = $(".modal__close");
@@ -51,87 +54,21 @@ $(document).ready(function () {
   closeBtn.on("click", function () {
     modal.toggleClass("modal--visible");
   });
+
+// Check at which height window top, display button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 650) {
+      $(".arrow-top").fadeIn();
+    } else {
+      $(".arrow-top").fadeOut();
+    }
+  });
+
+  // Click event to scroll top
+  $(".arrow-top").click(function () {
+    $("html, body").animate ({scrollTop: 0}, 1500);
+    return false;
+  });
+
 });
 
-
-// var name = "Serhii!";
-// var age = 17;
-
-// console.log(name + " " + age)
-
-// for (let i = 0; i <= 10; i++) {
-//   console.log(i);
-// }
-
-// var pass = false;
-// if (7 > 6) {
-//   console.log("Success!")
-// } else {
-//   console.log ("Wrong!");
-// }
-
-// var hero = document.querySelector(".hero");
-// console.log(hero);
-
-// console.log(typeof(age));
-
-// var serhii = {
-//   name: "Serhii",
-//   age: 17,
-//   hair: "brown",
-//   eyes: "breen",
-//   resident: "UKR/USA",
-//   address: "1220 State Street",
-//   social: 511551511,
-//   wife: {
-//     name: "Ezabella"
-//   }
-// }
-
-// console.log(serhii);
-// console.log(serhii.age)
-
-// for (var i = 0; i <= 10; i++) {
-//   console.log(i);
-// }
-
-// console.log("new value of i ",
-//  i);
-// console.log("new value of i " + i)
-
-// var itemsList = document.querySelectorAll('.card__items');
-
-// console.log(itemsList[0].classList)
-
-
-// itemsList.forEach(item => {
-//   item.classList.add("from-javascript");
-// });
-
-// function getSum(x, y) {
-//   console.log(x + y)
-// }
-
-// getSum(5, 10);
-
-// var hello = (name, age) => {
-//   console.log("Hello " + name + ". Your " + age + " age?");
-// };
-
-// hello("Serhii", 17);
-
-// button = document.querySelector(".logo");
-// console.log(button);
-
-// button.addEventListener("click", function (event) {
-//   console.log("This class is " + this.classList);
-// });
-
-// document.addEventListener("keypress", function(event) {
-//   // console.log("You press on " + event.key);
-//   if(event.code == "Space") {
-//     console.log("You clicked on space");
-//   } else {
-//     console.log("event.key");
-//   }
-// });
