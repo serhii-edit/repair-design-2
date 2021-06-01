982d16a0b378398cf27fddea6c751ed6771d6b90
Serhii Yakymenko <64821812+serhii-edit@users.noreply.github.com>
2021-05-31 21:57:18 -0400

    Refact: projects slider (in process) fixing

--- src/js/main.js
+++ src/js/main.js
@@ -46,6 +46,27 @@ document.addEventListener("DOMContentLoaded", function(event){
     document.getElementById("myBar").style.width = scrolled + "%";
   }
 
+  // Swiper START
+  const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    // loop: true,
+    slidesPerView: 1,
+
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+  // Swiper END
+
 });
 
 
