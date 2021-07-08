5da5a8a682b7e67f14e2a769b7d57566f2506d46
Serhii Yakymenko <64821812+serhii-edit@users.noreply.github.com>
2021-07-08 11:41:58 -0400

    Refact: [Fix] -SMTPSecure ssl- Notice: input none, varibale undefined index (line of variable)

--- src/mailSender.php
+++ src/mailSender.php
@@ -1,10 +1,10 @@
 <?php
-  
-  
+  // Has Notice when input not filled, then Notice: index undefined (line of variable).
 $userName = $_POST['userName'];
 $userEmail = $_POST['userEmail'];
 $userPhone = $_POST['userPhone'];
-
+$userQuestion = $_POST['userQuestion'];
+  
   require "PHPMailer/Exception.php";
   require "PHPMailer/PHPMailer.php";
   require "PHPMailer/SMTP.php";
@@ -20,24 +20,31 @@ try {
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'mailsenderyak@gmail.com';                     //SMTP username
     $mail->Password   = 'Mailsender0101#';                               //SMTP password
-    $mail->SMTPSecure = ssl;            //Enable implicit TLS encryption
+    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('m.serhiiyakymenko@gmail.com', 'Serhii Yakymenko');
-    $mail->addAddress('m.serhiiyakymenko@gmail.com', 'Serhii Yakymenko'); 
-    $mail->addAddress($userEmail, 'Repair Design'); 
+    $mail->setFrom('mailsenderyak@gmail.com', 'Repair Design');
+    $mail->addAddress('m.serhiiyakymenko@gmail.com', 'Serhii Yakymenko');
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Repair Design Request';
     $mail->Body    = "
-    <h3/>Data of Requester</h3><br><br>
-    Name: <b>$userName</b><br />
-    Telephone-number: <b>$userPhone</b><br />
-    Email: <b>$userEmail</b><br />
-
-    <a href='serhii-yakymenko.zzz.com.ua/webs/repair-design-2'>Sent from serhii-yakymenko.zzz.com.ua/webs/repair-design-2/</a>
+    <span style='display: block; max-width: 600px; margin: 0 auto;'>
+    <h3 style='color: #000;'>Data of Requester</h3>
+    <br />
+    Name: <b>$userName</b>
+    <br />
+    Telephone-number: <b>$userPhone</b>
+    <br />
+    Email: <b>$userEmail</b>
+    <br />
+    Question: $userQuestion
+    <br />
+    <br />
+    <a style='color: #000; text-decoration: none; cursor: pointer;' href='serhii-yakymenko.zzz.com.ua/webs/repair-design-2'>Sent from: <b>serhii-yakymenko.zzz.com.ua/webs/repair-design-2</b></a>
+    </span>
     ";
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
